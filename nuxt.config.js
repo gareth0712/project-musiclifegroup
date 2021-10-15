a80d160f610ca9744f467176cbd8ac09122c36b4
gareth0712 <gareth_0712@hotmail.com>
2021-10-15 21:22:35 +0800

    Update site title, description and nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,39 @@
-
 export default {
-  mode: 'universal',
+  ssr: false,
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
     link: [
-      { rel: 'apple-touch-icon', sizes:'57x57', type: 'image/x-icon', href: '/images/favicon/apple-icon-57x57.png' },
-      { rel: 'apple-touch-icon', sizes:'60x60', type: 'image/x-icon', href: '/images/favicon/apple-icon-60x60.png' },
-      { rel: 'apple-touch-icon', sizes:'72x72', type: 'image/x-icon', href: '/images/favicon/apple-icon-72x72.png' },
-      { rel: 'icon', sizes:'32x32', type: 'image/png', href: '/images/favicon/favicon-32x32.png' },
-      { rel: 'icon', sizes:'16x16', type: 'image/png', href: '/images/favicon/favicon-16x16.png' },
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        type: "image/x-icon",
+        href: "/images/favicon/apple-icon-57x57.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        type: "image/x-icon",
+        href: "/images/favicon/apple-icon-60x60.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        type: "image/x-icon",
+        href: "/images/favicon/apple-icon-72x72.png",
+      },
+      { rel: "icon", sizes: "32x32", type: "image/png", href: "/favicon.ico" },
       { rel: "stylesheet", href: "/css/bootstrap.min.css" },
       { rel: "stylesheet", href: "/css/animate.css" },
       { rel: "stylesheet", href: "/css/font-awesome.min.css" },
@@ -25,46 +42,41 @@ export default {
       { rel: "stylesheet", href: "/plugins/mediabox/mediabox.min.css" },
       { rel: "stylesheet", href: "/plugins/mediabox/mediabox.min.css" },
       { rel: "stylesheet", href: "/css/style.css" },
-      { rel: "stylesheet", href: "/css/responsive.css" }
+      { rel: "stylesheet", href: "/css/responsive.css" },
     ],
 
     script: [
       { src: "/assets/plugins/mediabox/mediabox.min.js", body: true },
       { src: "/assets/plugins/accordion/accordion.min.js", body: true },
-    ]
+    ],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#a461e5', height: '4px' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#a461e5", height: "4px" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
-}
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
