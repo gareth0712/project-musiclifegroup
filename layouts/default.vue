767bb6f4577c9028b23d2b630bcc761fdad87ab6
gareth0712 <gareth_0712@hotmail.com>
2021-10-18 09:48:58 +0800

    Update page title

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,25 +4,21 @@
   </div>
 </template>
 
-
 <script>
-
-  export default {
-    components: {
-    },
-    head(){
-      return {
-        title: "Lums - Vue Nuxt SEO Landing Template"
-      }
-    },
-    mounted(){
-      this.$nextTick(() => {
-        this.$nuxt.$loading.start()
-        setTimeout(() => this.$nuxt.$loading.finish(), 500)
-      })
-    }
-  }
+export default {
+  components: {},
+  head() {
+    return {
+      title: "生命之樂 - Music Life Group",
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.$nuxt.$loading.start();
+      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+    });
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
